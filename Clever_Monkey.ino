1c1166aab921121b338b2a9f61ec7f97d95cca8d
jonchristie <jonchristie2000@yahoo.co.uk>
2016-11-02 14:33:08 +0000

    Add files via upload

---
+++ Clever_Monkey.ino
@@ -0,0 +1,62 @@
+/*
+  State change detection (edge detection)
+
+ Often, you don't need to know the state of a digital input all the time,
+ but you just need to know when the input changes from one state to another.
+ For example, you want to know when a button goes from OFF to ON.  This is called
+ state change detection, or edge detection.
+
+
+
+ */
+
+// this constant won't change:
+const int  buttonPin = 2;    // the pin that the pushbutton is attached to
+const int ledPin = 7;       // the pin that the LED is attached to
+
+// Variables will change:
+int buttonPushCounter = 0;   // counter for the number of button presses
+int buttonState = 0;         // current state of the button
+int lastButtonState = 0;     // previous state of the button
+
+void setup() {
+  // initialize the button pin as a input:
+  pinMode(buttonPin, INPUT);
+  // initialize the LED as an output:
+  pinMode(ledPin, OUTPUT);
+  // initialize serial communication:
+  Serial.begin(9600);
+}
+
+
+void loop() {
+  // read the pushbutton input pin:
+  buttonState = digitalRead(buttonPin);
+
+  // compare the buttonState to its previous state
+  if (buttonState != lastButtonState) {
+    // if the state has changed, increment the counter
+    if (buttonState == HIGH) {
+      // if the current state is HIGH then the button
+      // wend from off to on:
+      
+      Serial.println("switch down");
+      digitalWrite(ledPin,LOW);
+      
+    } else {
+      // if the current state is LOW then the button
+      // wend from on to off:
+      
+      Serial.println("switch up");
+      digitalWrite(ledPin, HIGH);
+      Serial.println("Homework!!!");
+      
+    }
+    // Delay a little bit to avoid bouncing
+    delay(50);
+  }
+  // save the current state as the last state,
+  //for next time through the loop
+  lastButtonState = buttonState;
+
+}
